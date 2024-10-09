b20babfeff8f8a115e25696ec5641c54ec17b750
k-37 <60838818+k-37@users.noreply.github.com>
2024-10-09 08:11:47 +0200

    Add functional test for homepage
    Used commands:
    
        ./bin/console make:test WebTestCase HomepageControllerTest
    
    To test everything is OK run:
    
        make test
    
    - https://symfony.com/doc/6.4/the-fast-track/en/17-tests.html#writing-functional-tests-for-controllers

---
+++ tests/HomepageControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomepageControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', '🏎️ let\'s goooooooo for the 🏁!');
+    }
+}
